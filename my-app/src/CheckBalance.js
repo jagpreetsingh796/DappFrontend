849dbea08ea494a65b4f51a6a5488a1b9fbd0335
Kirpalsinh9 <kirpalsinhji976@yahoo.com>
2019-07-07 02:10:15 -0400

    Solved some errors that were in OpenAccount Component

--- my-app/src/CheckBalance.js
+++ my-app/src/CheckBalance.js
@@ -1,50 +1,47 @@
 import React from "react"
 import axios from "axios"
 
-class CheckBalance extends React.Component
-{
-    constructor()
-    {
+class CheckBalance extends React.Component {
+    constructor() {
         super()
-        this.state={
-            name:"",
-            balance:""
+        this.state = {
+            name: "",
+            balance: ""
         }
-        this.handleChange=this.handleChange.bind(this)
-        this.handleSubmit=this.handleSubmit.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
-    handleChange=e=>{
+    handleChange = e => {
         this.setState({
-            name:e.target.value
-        },console.log(e.target.value))
+            name: e.target.value
+        })
     }
-    handleSubmit= async (e)=>{
+    handleSubmit = async (e) => {
         e.preventDefault();
-        const res=await axios.post("http://localhost:3003/getbal",{
-            name:this.state.name,
+        const res = await axios.post("http://localhost:3003/getbal", {
+            name: this.state.name,
         }
         )
-        console.log("the balance is",res.data)
+
         this.setState({
-            balance:res.data
+            balance: res.data
         })
 
     }
 
-    render()
-    {
-        return(
+    render() {
+        return (
             <form onSubmit={this.handleSubmit}>
                 CheckBalance
-                <br/>
+                <br />
                 Enter Name
                 <input type="text" name="name" onChange={this.handleChange} value={this.state.name} />
                 <button type="submit">submit</button>
-                <br/>
-                
-                {this.state.balance!==""?<h3>The balance is {this.state.balance}</h3>:""}
+                <br />
 
-           </form>
+                {this.state.balance !== "" ? <h3>The balance is {this.state.balance}</h3> : ""}
+
+            </form>
         )
     }
 }
--- my-app/src/Withdraw.js
+++ my-app/src/Withdraw.js
@@ -2,66 +2,63 @@ import React from "react"
 import axios from "axios"
 import firebase from './Firestore'
 
-class Withdraw extends React.Component
-{
-    constructor()
-    {
+class Withdraw extends React.Component {
+    constructor() {
         super()
-        this.state={
-           name:"",
-           remBalance:"",
-           amount:""
+        this.state = {
+            name: "",
+            remBalance: "",
+            amount: ""
         }
-        this.handleChange=this.handleChange.bind(this)
-        this.handleSubmit=this.handleSubmit.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
-    handleChange=e=>{
+    handleChange = e => {
         this.setState({
-            [e.target.name]:e.target.value
-        },console.log(e.target.value))
+            [e.target.name]: e.target.value
+        })
     }
-    handleSubmit= async (e)=>{
+    handleSubmit = async (e) => {
         e.preventDefault();
-        const res=await axios.post("http://localhost:3003/withdraw",{
-            name:this.state.name,
-            amount:this.state.amount
+        const res = await axios.post("http://localhost:3003/withdraw", {
+            name: this.state.name,
+            amount: this.state.amount
         }
         )
         const db = firebase.firestore();
 
-  
-  const userRef = await db.collection("Balances").where('Name','==',this.state.name).get()
-    userRef.docs.forEach(async(Ele)=>{
-        console.log("the id is",Ele.id)
-        await db.collection("Balances").doc(Ele.id).update({
-            Balance:res.data
+
+        const userRef = await db.collection("Balances").where('Name', '==', this.state.name).get()
+        userRef.docs.forEach(async (Ele) => {
+            console.log("the id is", Ele.id)
+            await db.collection("Balances").doc(Ele.id).update({
+                Balance: res.data
+            })
         })
-    })
-  
-        console.log("the balance is",res.data)
+
+
         this.setState({
-            remBalance:res.data
+            remBalance: res.data
         })
 
     }
 
-    render()
-    {
-        return(
+    render() {
+        return (
             <form onSubmit={this.handleSubmit}>
-               Withdraw
-                <br/>
+                Withdraw
+                <br />
                 Enter Name
                 <input type="text" name="name" onChange={this.handleChange} value={this.state.name} />
                 Enter Amount in wei
                 <input type="text" name="amount" onChange={this.handleChange} value={this.state.amount} />
 
                 <button type="submit">submit</button>
-                <br/>
-                
-                {this.state.remBalance!==""?<h3>The remaining balance is {this.state.remBalance}</h3>:""}
+                <br />
 
-           </form>
+                {this.state.remBalance !== "" ? <h3>The remaining balance is {this.state.remBalance}</h3> : ""}
+
+            </form>
         )
     }
 }
